b33251dc2ad77c31b9e311dbc59edc9d84f0981c
Daita Sarkar <81379546+daitasarkar@users.noreply.github.com>
2021-03-30 16:15:54 +0530

    textarea and taking input

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ welcome="script is working "+username;
 alert(welcome);**/
 
 var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
 
 function clickEventHandler()
 {
     console.log("clicked");
+    console.log("input",txtInput.value);
 };
 
 btnTranslate.addEventListener("click",clickEventHandler);
