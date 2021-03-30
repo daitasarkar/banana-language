a10d5963d173832f7966c0793ae688574294331c
Daita Sarkar <81379546+daitasarkar@users.noreply.github.com>
2021-03-30 16:53:35 +0530

    input output

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ alert(welcome);**/
 
 var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+// to write text outputDiv.innerText="daita";
 
 function clickEventHandler()
 {
-    console.log("clicked");
-    console.log("input",txtInput.value);
+    //console.log("clicked");
+    //console.log("input",txtInput.value);
+    outputDiv.innerText="sjsjsjsj "+ txtInput.value;
 };
 
 btnTranslate.addEventListener("click",clickEventHandler);
